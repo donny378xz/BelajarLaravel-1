7266b24b350eff05a009a95ef34be2646855cc82
RizalAchp <81346706+RizalAchp@users.noreply.github.com>
2021-03-26 18:07:33 +0700

    update:26march

---
+++ resources/views/kahyangan.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> KAHYANGAN </title>
+    <style>
+      html, body {
+        background-color: #000;
+        color: #636b6f;
+        font-family: 'Nunito', sans-serif;
+        font-weight: 200;
+        height: 100vh;
+        margin: 0;
+      }
+
+      .full-height {
+        height: 100vh;
+      }
+
+      .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+      }
+
+      .position-ref {
+        position: relative;
+      }
+
+      .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+      }
+
+      .content {
+        text-align: center;
+      }
+
+      img {
+        max-width: 100%;
+        height: auto;
+      }
+
+      .m-b-md {
+        margin-bottom: 30px;
+      }
+      .title {
+        border-top: 2px solid;
+        font-size: 25px;
+        font-weight: bold;
+        text-transform: uppercase;
+        border-bottom: 2px solid;
+        padding: 12px 0;
+        color: #708090;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="flex-center position-ref full-height">
+      <img src="/img/aku-ganteng.jpg" alt="akuganteng" width="70%" height="auto">
+      <div class="content">
+        <img src="/img/kahyangan.png" alt="kahyangan" width="600" height="auto">
+        <div class="title m-b-md">
+            Kahyangan TKK C
+        </div>
+        <img src="/img/kahyanganbaummk.png" alt="baummk" width="600" height="auto">
+      </div>
+    </div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function ()
-{
+Route::get('/', function (){
     return view('welcome');
 });
-Route::get('/hello', function()
-{
+
+Route::get('/hello', function(){
 	return 'Hello World';
 });
 
-Route::get('/belajar', function()
-{
-	echo'<h1>Hello World</h1>';
-	echo '<p>Sedang belajar Laravel</p>';
+Route::get('/belajar',function(){
+	echo '<h1> Hello World </h1>';
+	echo '<p>Sedang Belajar laravel</p>';
 });
 
 Route::get('page/{nomor}', function($nomor){
-	return 'ini halaman ke-' .$nomor;
+	return 'Ini Halaman ke-' .$nomor;
+});
+
+Route::get('/kahyangan', function () {
+  return view('kahyangan');
 });
